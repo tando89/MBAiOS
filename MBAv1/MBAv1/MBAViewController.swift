3618125819953ead72084f75daa5985a5e9f4429
tan <tan@tans-Mac.local>
2017-06-28 00:40:23 -0700

    started v1

---
+++ MBAv1/MBAv1/MBAViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MBAViewController.swift
+//  MBAv1
+//
+//  Created by tan on 6/27/17.
+//  Copyright © 2017 tan. All rights reserved.
+//
+
+import UIKit
+
+class MBAViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
