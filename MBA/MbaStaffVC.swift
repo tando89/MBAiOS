5c0e953f2dbb9ab136c85028eb83b0b49f4f7c29
tan <tan@tans-Mac.local>
2017-06-30 17:54:17 -0700

    finished MBA staff info

--- MBA/MbaStaffVC.swift
+++ MBA/MbaStaffVC.swift
@@ -33,4 +33,28 @@ class MbaStaffVC: UIViewController {
         self.present(alert01, animated: true, completion: nil)
     }
     
+    @IBAction func drSealTel(_ sender: Any) {
+        let telNum = URL(string: "tel://9095375735"); UIApplication.shared.open(telNum!, options: [:], completionHandler: nil);
+    }
+    
+    @IBAction func drSealEmail(_ sender: Any) {
+        let drsealEmail = URL(string: "message://cseal@csusb.edu"); UIApplication.shared.open(drsealEmail!, options: [:], completionHandler: nil);
+    }
+    
+    
+    @IBAction func deborahDesc(_ sender: Any) {
+        let alert02 = UIAlertController(title: "Deborah Grijalva", message: "Greetings Prospective and Current Students, As the MBA Graduate Coordinator, I’m here to assist you in many different ways to ensure your success in your program of study. My purpose is to provide you with advisement services on a range of academic and related issues.", preferredStyle: .alert)
+        alert02.addAction(UIAlertAction(title: "Close", style: .cancel, handler: {(action:UIAlertAction) in
+        }))
+        self.present(alert02, animated: true, completion: nil)
+    }
+    
+    
+    
+    @IBAction func kasandraDesc(_ sender: Any) {
+        let alert03 = UIAlertController(title: "Kasandra Adams", message: "Hello FUTURE MBAs! I chose to pursue my MBA here at CSUSB. Now, as an alumnus, I am here to help you decide if that same decision, is right for YOU! Please reach out to me with any questions or concerns you have regarding the MBA program.", preferredStyle: .alert)
+        alert03.addAction(UIAlertAction(title: "Close", style: .cancel, handler: {(action:UIAlertAction) in
+        }))
+        self.present(alert03, animated: true, completion: nil)    }
+    
 }
